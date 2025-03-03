43dfbe152d111de7e650be271bd688e98c13a7fa
yago rocha <yagodasilvarocha30@gmail.com>
2025-03-03 17:44:32 -0300

    Obtendo um usuario por id

--- src/main/java/com/yagorocha/workshopmongo/services/UserService.java
+++ src/main/java/com/yagorocha/workshopmongo/services/UserService.java
@@ -1,7 +1,10 @@
 package com.yagorocha.workshopmongo.services;
 import java.util.List;
+
+
 import com.yagorocha.workshopmongo.domain.User;
 import com.yagorocha.workshopmongo.repository.UserRepository;
+import com.yagorocha.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
     }
 
 }
